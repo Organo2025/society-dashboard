0957f4debc0604cdc0c14bc67a9ac0076d02520d
Organo2025 <KONNDABABBW@GMAIL.COM>
2026-07-11 23:57:36 +0530

    Add initial script to display resource metrics

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("temperature").innerHTML="32°C";
+
+document.getElementById("water").innerHTML="48 KL";
+
+document.getElementById("electricity").innerHTML="3850 kWh";
+
+document.getElementById("diesel").innerHTML="120 L";
+
+document.getElementById("tank").innerHTML="84%";
+
+document.getElementById("stp").innerHTML="42 KL";
+
+document.getElementById("solar").innerHTML="220 kWh";
